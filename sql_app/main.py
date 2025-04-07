205acc7839447cc32a93290a9c138635e443eb48
devIgf <igfdev0@gmail.com>
2025-04-07 10:51:00 +0000

    compréhension des 5 fichiers

--- sql_app/main.py
+++ sql_app/main.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
+
+app = FastAPI()
+
+
 models.Base.metadata.create_all(bind=engine)
 
 #DEPENDENCIES
@@ -30,8 +34,23 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db)) -> schemas.UserCreate | HTTPException:
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return db_user 
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item, status_code=201) 
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/users/{user_id}/items/", response_model=list[schemas.Item])
+def read_items(
+    user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
+    items = crud.get_items(db, user_id=user_id, skip=skip, limit=limit)
+    return items
